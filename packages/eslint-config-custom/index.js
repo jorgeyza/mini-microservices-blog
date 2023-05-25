13a1a9686ae61b3b5559c05b72885b15c7dfcdaf
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-05-25 12:04:30 -0500

    Update client with shadcn next template

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,22 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  $schema: "https://json.schemastore.org/eslintrc",
+  extends: ["next/core-web-vitals", "turbo", "prettier", "plugin:tailwindcss/recommended"],
+  plugins: ["tailwindcss"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+    "tailwindcss/no-custom-classname": "off",
   },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
+  settings: {
+    tailwindcss: {
+      callees: ["cn"],
+      config: "./apps/client/tailwind.config.js",
     },
   },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
 };

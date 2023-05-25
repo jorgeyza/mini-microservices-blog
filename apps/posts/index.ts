f149fc42f3bd84aa0a21a55e0785117a5ad32add
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-05-24 21:39:27 -0500

    Add routes for posts

--- apps/posts/index.ts
+++ apps/posts/index.ts
@@ -1,4 +1,14 @@
-import Fastify from "fastify";
+import { randomBytes } from "crypto";
+import Fastify, { FastifyRequest, FastifyReply } from "fastify";
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface Posts {
+  [postId: string]: Post;
+}
 
 const fastify = Fastify({
   logger: {
@@ -8,10 +18,38 @@ const fastify = Fastify({
   },
 });
 
+// No db, all posts will be in memory.
+const posts: Posts = {};
+
+fastify.get("/api/posts", {
+  handler: (request: FastifyRequest, reply: FastifyReply) => {
+    return reply.send(posts);
+  },
+});
+
+fastify.get("/api/posts", {
+  handler: (
+    request: FastifyRequest<{
+      Body: Omit<Post, "id">;
+    }>,
+    reply: FastifyReply
+  ) => {
+    const id = randomBytes(4).toString("hex");
+    const { title } = request.body;
+
+    posts[id] = {
+      id,
+      title,
+    };
+
+    return reply.code(201).send(posts[id]);
+  },
+});
+
 async function main() {
   try {
     await fastify.listen({
-      port: Number(process.env.PORT),
+      port: 4000,
       host: "0.0.0.0",
     });
   } catch (error) {
